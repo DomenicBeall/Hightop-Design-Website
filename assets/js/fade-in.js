1c94f548455c57879f2dd1353f84c1b3e8de2a21
Domenic Beall <domenicbeall2@gmail.com>
2021-05-03 14:52:01 +1000

    Potential fix for shoe slide in issue

--- assets/js/fade-in.js
+++ assets/js/fade-in.js
@@ -1,3 +1,4 @@
 window.sr = new ScrollReveal();
 sr.reveal(".fade-in", { origin: 'bottom', distance: '50px', duration: 1000 });
+sr.reveal(".opaque-fade-in", { origin: 'right', distance: '50px', duration: 1000, opacity: 1});
 
